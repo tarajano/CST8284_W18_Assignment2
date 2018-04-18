61c04f88de7a5451958b17e61a7bc60731f4cd14
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-04-18 13:21:40 -0400

    Ads text shadow effect.
    FILE:   src/cst8284/triviatime/AnimEffects.java
      ADDS: dropped shadow for logo text using text.setEffect()

--- src/cst8284/triviatime/AnimEffects.java
+++ src/cst8284/triviatime/AnimEffects.java
@@ -8,6 +8,8 @@ import javafx.animation.ParallelTransition;
 import javafx.animation.ScaleTransition;
 import javafx.animation.Timeline;
 import javafx.scene.Group;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -21,43 +23,46 @@ import javafx.util.Duration;
  */
 public class AnimEffects {
   
-//  private static Stage stage;
-  
-  //ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(2000), new Text());
-  
-  
-  public static Group insertFadeInLogo(String text) {
-    Group g = new Group();  
-    Text t = new Text();
-    t.setFill(Color.BLUE);
-    t.setText(text);
-    t.setFont(Font.font(null, FontWeight.BOLD, 48));
-    FadeTransition ft = new FadeTransition(Duration.millis(8000), t);
-    ft.setFromValue(.05);
-    ft.setToValue(5);
-    ft.setAutoReverse(true);
-    ft.play();
-    g.getChildren().add(t);
-    return g;
-  }
+//  public static Group insertFadeInLogo(String text) {
+//    Group g = new Group();  
+//    Text t = new Text();
+//    t.setFill(Color.BLUE);
+//    t.setText(text);
+//    t.setFont(Font.font(null, FontWeight.BOLD, 48));
+//    FadeTransition ft = new FadeTransition(Duration.millis(8000), t);
+//    ft.setFromValue(.05);
+//    ft.setToValue(5);
+//    ft.setAutoReverse(true);
+//    ft.play();
+//    g.getChildren().add(t);
+//    return g;
+//  }
 
   public static Group insertFadeInScaledLogo(String text) {
     Group g = new Group();
+    
+    // Text Shadow
+    DropShadow dropShadow = new DropShadow();
+    dropShadow.setRadius(5.0);
+    dropShadow.setOffsetX(3.0);
+    dropShadow.setOffsetY(3.0);
+    dropShadow.setColor(Color.GRAY);
     
     // Text
     Text logoText = new Text();
     logoText.setFill(Color.BLUE);
     logoText.setText(text);
     logoText.setFont(Font.font(null, FontWeight.BOLD, 48));
+    logoText.setEffect(dropShadow);
     
     // FadeIn
-    FadeTransition fadeTrans = new FadeTransition(Duration.millis(8000), logoText);
-    fadeTrans.setFromValue(.05);
+    FadeTransition fadeTrans = new FadeTransition(Duration.millis(4000), logoText);
+    fadeTrans.setFromValue(.025);
     fadeTrans.setToValue(5);
     fadeTrans.setAutoReverse(true);
     
     // ZoomIn
-    ScaleTransition scaleTrans = new ScaleTransition(Duration.millis(4000), logoText);
+    ScaleTransition scaleTrans = new ScaleTransition(Duration.millis(5000), logoText);
     scaleTrans.setToX(1.2f);
     scaleTrans.setToY(1.2f);
     scaleTrans.setCycleCount(1);
